46860f23d84515856117e4efd01fda1e97d80ca9
gilangr1440 <gilangr1440@gmail.com>
2024-05-24 09:47:39 +0700

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Home from "@/pages/Home";
 import Profile from "@/pages/Profile";
 import DaftarPenginapan from "@/pages/DaftarPenginapan";
-import DetailPenginapan from "@/pages/DetailPenginapan";
 import DaftarReservasi from "@/pages/DaftarReservasi";
 import NoPage from "@/pages/NoPage";
-import DetailPenginapan from "./pages/DetailPenginapan";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import History from "./pages/History";
+import Review from "./pages/Review";
 
 const App = () => {
   return (
@@ -21,15 +18,13 @@ const App = () => {
         <Route path="/penginapan/:id_penginapan" element={<DetailPenginapan />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/daftar-penginapan" element={<DaftarPenginapan />} />
-        <Route
-          path="/penginapan/:id_penginapan"
-          element={<DetailPenginapan />}
-        />
+        <Route path="/penginapan/:id_penginapan" element={<DetailPenginapan />} />
         <Route path="/daftar-reservasi" element={<DaftarReservasi />} />
         <Route path="*" element={<NoPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/history" element={<History />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </BrowserRouter>
   );
